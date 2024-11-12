d28f551f3d07070b085ad4fb8299d58e87b9b353
moryno <mauricenganga41@gmail.com>
2024-11-12 07:43:27 +0300

    First commit and first setup of both the frontend and backend

---
+++ backend/server.mjs
@@ -0,0 +1,22 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected to blogDB");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+app.use(express.json());
+
+app.listen(process.env.PORT || 8800, () => {
+  connect();
+  console.log("Backend running at port 8800");
+});
