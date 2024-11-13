0c2e91c38f8cf528f20cfe24fbefefa1b87eacbd
moryno <mauricenganga41@gmail.com>
2024-11-13 08:17:16 +0300

    Add crud for post and register and login schema and resolver for the user

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { graphqlHTTP } from "express-graphql";
+import graphqlSchema from "./src/graphql/schema.js";
+import graphqlResolver from "./src/graphql/resolvers.js";
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected to blogDB");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+app.use(express.json());
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
+    graphiql: true,
+  })
+);
+
+app.listen(process.env.PORT || 8080, () => {
+  connect();
+  console.log("Backend running at port 8080");
+});
